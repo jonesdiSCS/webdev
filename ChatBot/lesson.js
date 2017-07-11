d7360be19ed310b20e02f405c748bbafcb655459
DJ <epicmrjones@gmail.com>
2017-07-11 15:46:58 -0500

    Added folder
    Added folder to project and saved files

--- ChatBot/lesson.js
+++ ChatBot/lesson.js
@@ -1,22 +1,22 @@
-// var x = 1;
-// var y = 'yellow';
-// var z = "zebra";
+var x = 1;
+var y = 'yellow';
+var z = "zebra";
 
-// var here = 4, there = 5, another1 = 'dj', _name = "cedric";
+var here = 4, there = 5, another1 = 'dj', _name = "cedric";
 
-// console.log('this is a message');
-// console.log(x + here + y);
+console.log('this is a message');
+console.log(x + here + y);
 
-// // alert('hello world. Who are you?');
-// // var me = prompt();
-// // alert('hello ' + me);
+// alert('hello world. Who are you?');
+// var me = prompt();
+// alert('hello ' + me);
 
-// //if, else
-// // if (me == 'DJ') {
-// //     console.log('this is true so you see this message');
-// // } else {
-// //     console.log('the original case was false so you see this message');
-// // }
+//if, else
+// if (me == 'DJ') {
+//     console.log('this is true so you see this message');
+// } else {
+//     console.log('the original case was false so you see this message');
+// }
 
 // //while
 // while (x <= 2) {
